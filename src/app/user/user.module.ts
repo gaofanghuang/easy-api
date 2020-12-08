f2d3d910f2c552ad67c082f9a0fb447e2d4609e5
gaofang huang <gaofanghuang@vip.qq.com>
2020-12-08 16:32:40 +0800

    修复无法登录的问题

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,15 +3,24 @@ import { DatabaseModule } from 'src/service/database/database.module';
 import { UserProviders } from './user.providers';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { AuthModule } from 'src/app/user/auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/config/jwtSecret';
 
 @Module({
   imports: [
-    AuthModule,
     DatabaseModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      // token 过期时效
+      signOptions: { expiresIn: '8h' },
+    }),
   ],
   controllers: [UserController],
-  providers: [...UserProviders, UserService],
-  exports: [UserService],
+  providers: [...UserProviders, UserService, AuthService, JwtStrategy],
+  exports: [UserService, AuthService],
 })
 export class UserModule {}
